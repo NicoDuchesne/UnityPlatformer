f71c9fc9237d8ed3af17fc04c4f83cef7ecc03fd
NicoDuchesne <nicolas.duchesne@edu.devinci.fr>
2024-05-03 10:46:52 +0200

    add camera transitions
    on ajoute les transitions lorsqu'on change entre les camera profiles pour que ce soit plus smooth

--- SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraManager : MonoBehaviour
 {
@@ -10,6 +11,10 @@ public class CameraManager : MonoBehaviour
     [Header("Profile System")]
     [SerializeField] private CameraProfile _defaultCameraProfile;
     private CameraProfile _currentCameraProfile;
+    private float _profileTransitionTimer = 0f;
+    private float _profileTransitionDuration = 0f;
+    private Vector3 _profileTransitionStartPosition;
+    private float _profileTransitionStartSize;
 
     private void Awake()
     {
@@ -22,19 +27,40 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        _SetCameraPosition(_currentCameraProfile.Position);
-        _SetCameraSize(_currentCameraProfile.CameraSize);
+        if (_IsPlayingProfileTransition())
+        {
+            _profileTransitionTimer += Time.deltaTime;
+
+            Vector3 transitionPosition = _CaluclateProfileTransitionPosition(_currentCameraProfile.Position);
+            _SetCameraPosition(transitionPosition);
+
+            float transitionSize = _CaluclateProfileTransitionCameraSize(_currentCameraProfile.CameraSize);
+            _SetCameraSize(transitionSize);
+        } else
+        {
+            _SetCameraPosition(_currentCameraProfile.Position);
+            _SetCameraSize(_currentCameraProfile.CameraSize);
+        }
+        
     }
 
-    public void EnterProfile(CameraProfile cameraProfile)
+    public void EnterProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
     {
         _currentCameraProfile = cameraProfile;
+        if (transition != null)
+        {
+            _PlayProfileTransition(transition);
+        }
     }
 
-    public void ExitProfile(CameraProfile cameraProfile)
+    public void ExitProfile(CameraProfile cameraProfile, CameraProfileTransition transition = null)
     {
         if (_currentCameraProfile != cameraProfile) return;
         _currentCameraProfile = _defaultCameraProfile;
+        if (transition != null)
+        {
+            _PlayProfileTransition(transition);
+        }
     }
 
     private void _InitToDefaultProfile()
@@ -57,5 +83,32 @@ public class CameraManager : MonoBehaviour
         _camera.orthographicSize = size;
     }
 
-    
+    private void _PlayProfileTransition(CameraProfileTransition transition)
+    {
+        _profileTransitionStartPosition = _camera.transform.position;
+        _profileTransitionStartSize = _camera.orthographicSize;
+
+        _profileTransitionTimer = 0f;
+        _profileTransitionDuration = transition.duration;
+    }
+
+    private bool _IsPlayingProfileTransition()
+    {
+        return _profileTransitionTimer < _profileTransitionDuration;
+    }
+
+    private float _CaluclateProfileTransitionCameraSize(float endSize)
+    {
+        float percent = _profileTransitionTimer / _profileTransitionDuration;
+        float startSize = _profileTransitionStartSize;
+        return Mathf.Lerp(startSize, endSize, percent);
+    }
+
+    private Vector3 _CaluclateProfileTransitionPosition(Vector3 destination)
+    {
+        float percent = _profileTransitionTimer / _profileTransitionDuration;
+        Vector3 origin = _profileTransitionStartPosition;
+        return Vector3.Lerp(origin, destination, percent);
+    }
+
 }
---
+++ SuperSquareLand-clean-version/Assets/SSL/Runtime/Scripts/Camera/CameraProfileTransition.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CameraProfileTransition {
+
+    public float duration = 0.5f;
+}
